4910ec9530486b74c381e2e7902da12a00edac00
cvara0 <cvara.parra@gmail.com>
2022-06-20 22:42:01 -0300

    20 jun 21 41

--- src/main/java/com/switch_and_trade/switch_and_trade_artifact/controladores/PublicacionControlador.java
+++ src/main/java/com/switch_and_trade/switch_and_trade_artifact/controladores/PublicacionControlador.java
@@ -1,26 +1,32 @@
 package com.switch_and_trade.switch_and_trade_artifact.controladores;
 
+import com.switch_and_trade.switch_and_trade_artifact.servicios.PropiedadServicio;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("/publicaciones")
 @RequiredArgsConstructor
 public class PublicacionControlador {
-    /*
-    * en este controlador se vana tomar datos de propiedades y vehiculos para mostarlos en conjunto*/
-    @GetMapping("/tabla-todo-publicacion-perfil")///{id}se busca en las publicaciones que tengan ese id de perfil
-    public ModelAndView tablaTodoPublicacionPerfil(/*@PathVariable Long idPerfil*/) {
+
+    private final PropiedadServicio propiedadServicio;
+    @GetMapping("/tabla-todo-publicacion-perfil")
+    public ModelAndView tablaTodoPublicacionPerfil(HttpSession session) {
         ModelAndView mav = new ModelAndView("tabla-todo-publicacion-perfil.html");
+        //tendria que agregar vehiculo a otra lista y mandarle tambien
+        mav.addObject("listaPropiedadPerfil",propiedadServicio.traerTodoPorIdPerfil((Long) session.getAttribute("id")));
         return mav;
     }
 
     @GetMapping("/tabla-todo-publicacion")
     public ModelAndView tablaTodoPropiedad() {//llaman al mismo html con vehiculo pero le mandan cosas diferentes
         ModelAndView mav = new ModelAndView("tabla-todo-publicacion.html");
-
+        mav.addObject("listaPropiedad",propiedadServicio.traerTodo());
         return mav;
     }
 
